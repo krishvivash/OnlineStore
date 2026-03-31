08a915044fe496da8b431a41edbe3ebdecde80a1
krishviash <krishvivash23@gmail.com>
2026-03-31 15:57:08 +1100

    products showing

--- books.js
+++ books.js
@@ -1,3 +1,39 @@
+function renderBooks() {
+  const booksWrapper = document.querySelector('.books');
+
+  const books = getBooks();
+
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="" />
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+    </div>
+    <div class="book__price">
+      <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+    </div>
+  </div>`
+  }).join("");
+
+  
+  booksWrapper.innerHTML = booksHtml
+  console.log(booksHtml)
+  //booksWrapper.innerHTML = 
+  
+}
+
+setTimeout(() => {
+  renderBooks();
+})
 
 
 // FAKE DATA
